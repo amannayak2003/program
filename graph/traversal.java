ac11fcf3a3318b3f5ef0e02bc58fcfcf0d5b3e36
amannayak2003 <amannayak774@gmail.com>
2023-11-13 12:46:50 +0530

    Traversal in Graph

---
+++ graph/traversal.java
@@ -0,0 +1,42 @@
+package graph;
+
+import java.util.*;
+
+
+public class traversal {
+
+
+    static class edge{
+        int src ;
+        int dest;
+        int weight;
+
+        public edge(int s ,int d , int w ){
+            this.src = s;
+            this.dest = d;
+            this.weight=w;
+        }
+    }
+
+    public static void bfs(ArrayList<edge>[] graph){
+        Queue<Integer> q = new LinkedList<>();
+        boolean vist[] = new boolean[graph.length]
+
+        q.add(0);
+        while(!q.isEmpty()){
+            int cur = q.remove();
+
+            if(vist[cur]!=true){
+                System.out.print(cur);
+                vist[cur] = true;
+                for(int i = 0 ; i<graph[cur].size();i++){
+                    edge e = graph[cur].get(i);
+                    q.add(e.dest);
+                }
+            }
+        }
+
+
+    }
+
+}
