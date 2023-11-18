eba7abc5aa72cd7e32aebdf187f98d630f62edda
amannayak2003 <amannayak774@gmail.com>
2023-11-18 21:20:17 +0530

    AddFirst method in LinkedList

--- LinkedList/linkedlist.java
+++ LinkedList/linkedlist.java
@@ -9,5 +9,29 @@ public class linkedlist {
             this.data = d;
             this.next = null;
         }
+    }
+    
+        public static node head;
+        public static node tail;
+
+
+        public void addfirst(int data){
+            // create new node 
+            node newnode = new node(data);
+            // if head points null
+            if(head==null){
+                head = tail = newnode;
+                return;
+            }
+            // new node next points to head  
+            newnode.next = head;
+            // new node becomes head
+            head = newnode;
+        }
+       public static void main(String[] args) {
+            linkedlist ll = new linkedlist();
+            ll.addfirst(1);
+            ll.addfirst(2);
+            
        }
 }
