38fe22dacb32a0ab6bb97b9110cb20d0ea011aab
amannayak2003 <amannayak774@gmail.com>
2023-11-18 21:07:01 +0530

    Create a linkedlist class

---
+++ LinkedList/linkedlist.java
@@ -0,0 +1,13 @@
+package LinkedList;
+
+public class linkedlist {
+       public static class node {
+        int data;
+        node next;
+
+        public node(int d){
+            this.data = d;
+            this.next = null;
+        }
+       }
+}
