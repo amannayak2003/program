c38e5557501954948dfad26954991f0e9ed47716
amannayak2003 <amannayak774@gmail.com>
2023-11-18 21:46:16 +0530

    print LinkedList

--- LinkedList/linkedlist.java
+++ LinkedList/linkedlist.java
@@ -10,7 +10,7 @@ public class linkedlist {
             this.next = null;
         }
     }
-    
+
         public static node head;
         public static node tail;
 
@@ -28,10 +28,40 @@ public class linkedlist {
             // new node becomes head
             head = newnode;
         }
+
+        public void addlast(int data){
+            // create new node 
+            node newnode = new node(data);
+            // if tail points null
+            if(head==null){
+                head = tail = newnode;
+                return;
+            }
+            // tail next points to new node 
+            tail.next = newnode;
+            // new node becomes head
+            tail = newnode;
+        }
+
+        public void printt(){
+            if(head==null){
+                System.out.println("linkedlist is empty");
+            }
+            else{
+                node temp = head;
+                while(temp!=null){
+                    System.out.print(temp.data+" ");
+                    temp = temp.next;
+                }
+            }
+        }
        public static void main(String[] args) {
             linkedlist ll = new linkedlist();
             ll.addfirst(1);
             ll.addfirst(2);
+            ll.addlast(3);
+            
+            ll.printt();
             
        }
 }
