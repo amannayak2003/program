3a2c58758db11d229ce85c0946531de8603552ec
amannayak2003 <amannayak774@gmail.com>
2023-12-09 09:50:44 +0530

    prefix problem

--- trie/create.java
+++ trie/create.java
@@ -2,11 +2,13 @@ public class create {
     static class node{
         node children[] = new node[26];
         boolean eow = false;
+        int freq;
 
         node(){
             for(int i = 0 ; i<26;i++){
                 children[i] = null;
             }
+            freq=1;
         }
     }
 
@@ -20,17 +22,53 @@ public class create {
             if(curr.children[idx]==null){
                 curr.children[idx] = new node();
             }
+            else{
+                curr.children[idx].freq++;
+            }
             curr = curr.children[idx];
         }
 
         curr.eow = true;
     }
+    // search
+    // public static boolean search(String key){
+    //     node curr = root;
+    //     for(int level = 0;level<key.length();level++){
+    //         int idx = key.charAt(level)-'a';
+    //         if(curr.children[idx]==null){
+    //             return false;
+    //         }
+    //         curr = curr.children[idx];
+    //     }
+
+    //     return curr.eow = true;
+    // }
     
+    public static void prefix(node r , String ans){
+        if(r==null){
+            return;
+        }
+        if(r.freq ==1){
+            System.out.println(ans);
+            return;
+        }
+
+        for(int i = 0 ;i<r.children.length;i++){
+            if(r.children!=null){
+                prefix(r.children[i], ans+(char)(i+'a'));
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        String words[] = {"the","a","there","three"};
+        String words[] = {"zebra","dog","duck","dove"};
         for(int i = 0;i<words.length;i++){
             insert(words[i]);
         }
+
+        root.freq=-1;
+        prefix(root, "");
+
+       
     }
 }
